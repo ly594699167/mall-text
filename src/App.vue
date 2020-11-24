f3da8c74f3d0a3b4ef3b0aaa31899d54bae7a047
xiaoyao <1754910927@qq.com>
2020-11-24 16:20:40 +0800

    整体布局

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Index/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Index from './components/index.vue';
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'app',
+  components: {
+    Index,
+  },
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style>
+@import url('~@/assets/rests.less');
 </style>
